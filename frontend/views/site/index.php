0d946a5fe2a692b3345333c92157e95fe40e9655
BeverlyKalwara <39458302+BeverlyKalwara@users.noreply.github.com>
2020-11-29 17:20:34 +0300

    Layout

---
+++ frontend/views/site/index.php
@@ -0,0 +1,154 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Sams Shoes';
+?>
+<div>
+    <div class="container newproduct">
+    <div class="corner-text">
+        <h1 class=hed-1>Built for<br> Flight</h1>
+        <p class=par-1>Checkout <strong>KASI</strong><br>The fastest shoe in the market</p>
+        <button type="button" class="btn btn-dark">SHOP</button>
+    </div>
+        
+    </div>
+    <div class="row-0">
+            <div class="row">
+                <div class="col-sm-3 card-1">
+                    <img class=""src="css/images/women11.jpg" alt="Card image cap">
+                    <div class="card-img-overlay">
+                    <h5 class="card-title-0">Women</h5>
+                    <p class="card-text">Shop Now<i class="fas fa-arrow-right"></i></p>
+                    </div>
+                </div>
+                <div class="col-sm-3 card-1">
+                    <img class="" src="css/images/men1.jpg" alt="Card image cap">
+                    <div class="card-img-overlay">
+                    <h5 class="card-title-0">Men</h5>
+                    <p class="card-text">Shop Now<i class="fas fa-arrow-right"></i></p>
+                    </div>
+                </div>
+                <div class="col-sm-3 card-1">
+                    <img class="" src="css/images/kids1.jpg" alt="Card image cap">
+                    <div class="card-img-overlay">
+                    <h5 class="card-title-0">Kids</h5>
+                    <p class="card-text">Shop Now<i class="fas fa-arrow-right"></i></p>
+                    </div>
+                </div>
+                <div class="col-sm-3 card-1">
+                    <img class="" src="css/images/accessories1.jpg" alt="Card image cap">
+                    <div class="card-img-overlay">
+                    <h5 class="card-title-0">Accessories</h5>
+                    <p class="card-text">Shop Now<i class="fas fa-arrow-right"></i></p>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    
+    <div class="newreleases">
+        <h2 class="hed-2"> New Releases</h2>
+        <div class="scrolling-wrapper">
+            <div class="card">
+                <img class="" src="css/images/shoes111.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes222.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes333.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes444.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div> 
+            <div class="card">
+                <img class="" src="css/images/shoes555.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes111.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes222.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>                                                                       
+        </div>
+    </div>
+        
+    <div class="newreleases">
+        <h2 class="hed-2"> Top picks</h2>
+        <div class="scrolling-wrapper">
+            <div class="card">
+                <img class="" src="css/images/shoes111.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes333.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes555.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes222.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div> 
+            <div class="card">
+                <img class="" src="css/images/shoes444.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes333.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="" src="css/images/shoes444.jpg" alt="Card image cap">
+                <div class="card-body">
+                <h5 class="card-title">Product name<br>2500KSH</h5> 
+                </div>
+            </div>                                                                       
+        </div>
+    </div> 
+     <div class="container">
+        <div class="mail-list">
+        	<h2 class="hed-2">Daily drip</h2>
+        	<p class="par-1">Receive updates about new<br>products and promotion</p>
+        	<button type="button" class="btn btn-outline-dark">JOIN MAILING LIST</button>
+        	
+        </div>     
+     </div>
+
+</div>
